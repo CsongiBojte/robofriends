ce5aedc80626b44283ecf5ec8a125c73aeed6e77
CsongiBojte <bojte.csongi12@gmail.com>
2023-04-07 12:27:46 +0300

    uploading robofriends project

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <div className="tc dib" style={{height:'5rem'}}>
+            <h2>This website is based on <a href="https://www.udemy.com/course/the-complete-web-developer-zero-to-mastery/">ZTM course</a></h2>
+        </div>
+    );
+};
+
+export default Footer;
